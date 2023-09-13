924a5150f287e5150fa2158bea7c4cbdadb511e5
WanjikuN <cikunjoroge4@gmail.com>
2023-09-13 11:34:30 +0300

    feat: add fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
+# import ipdb
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a= 0
+    b= 1
+    list_fib=[]
+    if(length == 0):
+        print(list_fib)
+    else:
+        for i in range(length):
+            c= a+b
+            list_fib.append(a)
+            a=b
+            b=c
+        print(list_fib)
+# ipdb.set_trace()
+print_fibonacci(1)
+
+
+  
